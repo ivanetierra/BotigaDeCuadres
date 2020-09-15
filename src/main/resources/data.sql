ff6a1e992bb45caedd98f83abb10b4651bd82f27
Ivan <ielizalde@gmail.com>
2020-09-15 11:03:27 +0200

    fix schema.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,47 +1,4 @@
--- MySQL Workbench Forward Engineering
-
-
-
--- -----------------------------------------------------
--- Schema mydb
--- -----------------------------------------------------
--- -----------------------------------------------------
--- Schema whitecollar
--- -----------------------------------------------------
-
--- -----------------------------------------------------
--- Schema whitecollar
--- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `whitecollar`;
-USE `whitecollar` ;
-
--- -----------------------------------------------------
--- Table `whitecollar`.`shop`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `whitecollar`.`shop` (
-  `idShop` INT(11) NOT NULL,
-  `name` VARCHAR(45) NULL DEFAULT NULL,
-  `maxCapacity` INT(11) NULL DEFAULT NULL,
-  PRIMARY KEY (`idShop`)
-  );
-
-
-
--- -----------------------------------------------------
--- Table `whitecollar`.`collar`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `whitecollar`.`collar` (
-  `idCollar` INT(11) NOT NULL,
-  `name` VARCHAR(45) NULL DEFAULT NULL,
-  `author` VARCHAR(45) NULL DEFAULT NULL,
-  `price` DECIMAL(10,0) NULL DEFAULT NULL,
-  `entryDate` DATE NULL DEFAULT NULL,
-  `Shop_idShop` INT(11) NOT NULL,
-   PRIMARY KEY (`idCollar`),
-
-  CONSTRAINT `fk_Collar_Shop`
-    FOREIGN KEY (`Shop_idShop`)
-    REFERENCES `whitecollar`.`shop` (`idShop`)
-    );
+insert into shop (idShop, name, maxCapacity) values(1,'Shop1',50);
+insert into shop (idShop, name, maxCapacity) values(2,'Shop2',100);
 
 
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,33 @@
+
+-- -----------------------------------------------------
+-- Schema whitecollar
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `whitecollar`;
+USE `whitecollar` ;
+
+-- -----------------------------------------------------
+-- Table `whitecollar`.`shop`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `whitecollar`.`shop` (
+  `idShop` INT(11) NOT NULL,
+  `name` VARCHAR(45) NULL DEFAULT NULL,
+  `maxCapacity` INT(11) NULL DEFAULT NULL,
+  PRIMARY KEY (`idShop`)
+  );
+
+-- -----------------------------------------------------
+-- Table `whitecollar`.`collar`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `whitecollar`.`collar` (
+  `idCollar` INT(11) NOT NULL,
+  `name` VARCHAR(45) NULL DEFAULT NULL,
+  `author` VARCHAR(45) NULL DEFAULT NULL,
+  `price` DECIMAL(10,0) NULL DEFAULT NULL,
+  `entryDate` DATE NULL DEFAULT NULL,
+  `Shop_idShop` INT(11) NOT NULL,
+   PRIMARY KEY (`idCollar`),
+
+  CONSTRAINT `fk_Collar_Shop`
+    FOREIGN KEY (`Shop_idShop`)
+    REFERENCES `whitecollar`.`shop` (`idShop`)
+    );
